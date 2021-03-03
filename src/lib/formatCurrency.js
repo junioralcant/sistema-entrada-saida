5e05a0092cb441af0730537d4ee7f7a3dffa8cdc
Junior Marques <junioralcant111@gmail.com>
2021-03-03 15:22:51 -0300

    Create format currency

---
+++ src/lib/formatCurrency.js
@@ -0,0 +1,14 @@
+const { formatToTimeZone } = require("date-fns-timezone");
+
+const formatCurrency = {
+  brl(value) {
+    value = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(value);
+
+    return value;
+  },
+};
+
+module.exports = formatCurrency;
